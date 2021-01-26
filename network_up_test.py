de7217a9c6431d449e4109fc5d428541a1af6937
Thomas Herold <thomasherold@outlook.com>
2021-01-26 21:26:26 +0100

    rework with csv writer and additional wifi and gateway test

--- network_up_test.py
+++ network_up_test.py
@@ -1,40 +1,45 @@
 import os
 import time
+import csv
 from datetime import datetime
 
-hostname = "google.com"
-filename = "demofile.txt"
-separator = ","
-up_cnt = 0
-down_cnt = 0
+hostname_gateway = "fritz.box"
+hostname_wifi = ""
+hostname_extern = "google.com"
+filename = "test1.csv"
+gateway_up_cnt = 0
+wifi_up_cnt = 0
+extern_up_cnt = 0
 check_cnt = 0
 
-def ping():
+def ping(hostname):
     return os.system("ping -c 1 " + hostname)
 
-def check_network():
-  file = open(filename,"a")
-  file.write(str(datetime.now()) + separator)
-  if ping() == 0:
-    file.write("up\n")
-    file.close()
-    return True
-  else:
-    file.write("down\n")
-    file.close()
-    return False
-
 def print_summary():
-  file = open(filename,"a")
-  file.write("------------------------\nUp: " + str(up_cnt) + " (" + str(up_cnt/check_cnt*100) + "%)\nDown: " + str(down_cnt) + " (" + str(down_cnt/check_cnt*100) + "%)\n")
-  file.close()
+  print("Gateway: " + str(gateway_up_cnt) + " (" + str(gateway_up_cnt/check_cnt*100) + "%)")
+  print("Wifi: " + str(wifi_up_cnt) + " (" + str(wifi_up_cnt/check_cnt*100) + "%)")
+  print("Extern: " + str(extern_up_cnt) + " (" + str(extern_up_cnt/check_cnt*100) + "%)")
 
 if __name__ == "__main__":
-  for i in range(0,5):
-    if True == check_network():
-      up_cnt += 1
-    else: 
-      down_cnt += 1
-    check_cnt += 1
-    time.sleep(1)
+  with open(filename,"w") as csvfile:
+    csv_writer = csv.writer(csvfile)
+    csv_writer.writerow(['date','gateway','wifi','extern'])
+    
+    for i in range(0,5):
+      gateway_check = False
+      wifi_check = False
+      extern_check = False
+      if ping(hostname_gateway) == 0:
+        gateway_check = True
+        gateway_up_cnt += 1
+      if ping(hostname_wifi) == 0:
+        wifi_check = True
+        wifi_up_cnt += 1
+      if ping(hostname_extern) == 0:
+        extern_check = True
+        extern_up_cnt += 1
+
+      csv_writer.writerow([datetime.now(),gateway_check,wifi_check,extern_check])
+      check_cnt += 1
+      time.sleep(60)
   print_summary()
