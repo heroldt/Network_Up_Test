c6772b9b0c179ac4a172da1918de84ddefb2b132
Thomas Herold <thomasherold@outlook.com>
2021-01-28 21:41:43 +0100

    added telegram bot and send daily summary

--- network_up_test.py
+++ network_up_test.py
@@ -1,6 +1,7 @@
 import os
 import csv
 from datetime import datetime
+import requests
 
 dt = datetime.now()
 hostname_gateway = "fritz.box"
@@ -11,6 +12,9 @@ gateway_up_cnt = 0
 wifi_up_cnt = 0
 extern_up_cnt = 0
 check_cnt = 0
+
+bot_token = ""
+bot_ID = ""
 
 def ping(hostname):
     return os.system("ping -c 1 " + hostname)
@@ -57,7 +61,22 @@ def read_csv():
         extern_up_cnt += 1
       check_cnt += 1
 
+def get_daily_summary():
+  title_text = "Zusammenfassung für gestern %02i.%02i.%i:\n" %(dt.day,dt.month,dt.year)
+  if check_cnt == gateway_up_cnt and check_cnt == wifi_up_cnt and check_cnt == extern_up_cnt:
+    summary_text = "Gestern gab es keine Ausfälle!"
+  else:
+    summary_text = "Gateway: %i (%.f%%)\nWifi: %i (%.f%%)\nExtern: %i (%.f%%)" %(
+      gateway_up_cnt,gateway_up_cnt/check_cnt*100,wifi_up_cnt,wifi_up_cnt/check_cnt*100,extern_up_cnt,extern_up_cnt/check_cnt*100)
+  return title_text + summary_text
+
+def send_message(message):
+  send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_ID + '&parse_mode=Markdown&text=' + message
+  response = requests.get(send_text)
+  return response.json()
+
 if __name__ == "__main__":
-    check_network()
-    read_csv()
-    print_summary()
+  check_network()
+  read_csv()
+  print_summary()
+  send_message(get_daily_summary())
